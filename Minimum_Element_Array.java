1b435a3078cf08ba13cabbef5a83a950620ae40d
prashantkr01 <prashantkr085@gmail.com>
2026-07-20 15:16:34 +0530

    Min Elemet

--- Minimum_Element_Array.java
+++ Minimum_Element_Array.java
@@ -8,7 +8,7 @@ public class Minimum_Element_Array {
         for(int i=0; i<arr.length; i++){
             arr[i]=sc.nextInt();
         }
-        int min=arr[0];55
+        int min=arr[0];
         for(int i=0; i<arr.length; i++){
             if(arr[i]<min){
                 min=arr[i];
