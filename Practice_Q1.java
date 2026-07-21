04ab06bac1f1593156960699953ab68b0d39a564
prashantkr01 <prashantkr085@gmail.com>
2026-07-21 11:30:27 +0530

    Practice Question No -1

---
+++ Practice_Q1.java
@@ -0,0 +1,14 @@
+//Multiply Odd Indexes elemnt by 2 and add 10 to even indexes elements?
+public class Practice_Q1 {
+    public static void main(String[] args) {
+        int[] arr={5,74,88,95,74};
+
+        for(int i=0; i<arr.length; i++){
+            if(i%2==0){
+                System.out.print(2*arr[i]+" ");
+            }else{
+                System.out.print(arr[i]+10+" ");
+            }
+        }
+    }
+}
