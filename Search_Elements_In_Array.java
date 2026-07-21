749b9d0fb4dc3a4202b5fda7811cf4e3e229ef5f
prashantkr01 <prashantkr085@gmail.com>
2026-07-21 12:14:07 +0530

    Search Element in Array

--- Search_Elements_In_Array.java
+++ Search_Elements_In_Array.java
@@ -1,16 +1,16 @@
 public class Search_Elements_In_Array {
     public static void main(String[] args) {
-        int x=20;
-        boolean a = false;
+        int target =20;
+        boolean flag = false;
         int[] arr={10,20,30,40,50};
 
         for(int i=0; i<arr.length; i++){
-            if(x==arr[i]){
-             a=true;
+            if(arr[i]==target){
+             flag=true;
 
             }
         }
-        if(a == true)
+        if( flag== true)
             System.out.println("Element Found");
         else
             System.out.println("Elemets Not Found");
