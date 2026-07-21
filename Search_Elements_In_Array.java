906e5a921580bcc3ef8b239300dcb5e512117ec7
prashantkr01 <prashantkr085@gmail.com>
2026-07-21 12:15:03 +0530

    Search Element In Array

--- Search_Elements_In_Array.java
+++ Search_Elements_In_Array.java
@@ -7,7 +7,7 @@ public class Search_Elements_In_Array {
         for(int i=0; i<arr.length; i++){
             if(arr[i]==target){
              flag=true;
-
+             break;
             }
         }
         if( flag== true)
