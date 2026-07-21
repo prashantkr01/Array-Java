8e869370565b028c421112a6944a4ff3d5d44e86
prashantkr01 <prashantkr085@gmail.com>
2026-07-21 14:56:55 +0530

    Second Maximum Element in Array

---
+++ Second_Maximum_Element_Array.java
@@ -0,0 +1,20 @@
+public class Second_Maximum_Element_Array {
+    public static void main(String[] args) {
+        int[] arr={44,52,87,96,37,44};
+
+        int max=Integer.MIN_VALUE;
+        int secmax=Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>secmax && arr[i]!=max){
+            secmax=arr[i];
+            }
+        }
+        System.out.println("Second Maximun = "+secmax);
+    }
+}
