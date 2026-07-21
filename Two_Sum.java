98ac446939d0c3684139c1358692d54c32064b03
prashantkr01 <prashantkr085@gmail.com>
2026-07-21 13:59:53 +0530

    Two Sum (Time Limit Exceed / Not Optimal Solution)

---
+++ Two_Sum.java
@@ -0,0 +1,14 @@
+public class Two_Sum {
+    public static void main(String[] args) {
+        int target=2;
+        int [] arr={1,3,4,6,-3,5};
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]+arr[j]==target){
+                    System.out.println(target);
+                }
+            }
+        }
+    }
+}
