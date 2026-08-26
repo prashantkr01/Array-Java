ac2253c02b336e83a5beb98e2f2b2f9a99da9e9d
prashantkr01 <prashantkr085@gmail.com>
2026-08-26 23:01:24 +0530

    Merge two sorted array

--- Merge_2_Sort_Array.java
+++ Merge_2_Sort_Array.java
@@ -1,8 +1,35 @@
 //Merge two sorted array in a single big array
 public class Merge_2_Sort_Array {
     public static void main(String[] args) {
-        
-    int [] a={2,4,6,8};
-    int [] b={1,3,5,7,9}; 
+    int [] a={2,4,6,9,20};
+    int [] b={1,3,4,5,7,8}; 
+    int [] arr= new int[a.length+b.length];
+    int i=0,j=0,k=0;
+    
+    while(i<a.length && j<b.length){
+        if(a[i]<b[j]){
+            arr[k]=a[i];
+            i++;
+            k++;
+        }else{
+            arr[k]=b[j];
+            j++;
+            k++;
+        }   
+        }
+        while(i<a.length){
+            arr[k]=a[i];
+            i++;
+            k++;
+        }
+        while(j<b.length){
+            arr[k]=b[j];
+            j++;
+            k++;
+        }
+
+    for(int ele : arr){
+        System.out.print(ele+" ");
+    }
 }
 }
